33aa27aa4ab1dfa8734a6821a01e9ecc075ea8ef
Bryan <b.yao@utexas.edu>
2020-05-30 17:15:55 -0500

    updated plotting for env, small bugs need fix

--- env_gen.py
+++ env_gen.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
 
 
 def gen_line_2_points(x1, y1, x2=None, y2=None, rise=None, run=None, rev=False):
@@ -17,10 +18,10 @@ def gen_line_2_points(x1, y1, x2=None, y2=None, rise=None, run=None, rev=False):
 def gen_triangle_2_points(x1, y1, x2, y2, x_bounds, y_bounds):
     constraints = []
     which_small = bool(random.getrandbits(1))
-    width1 = np.random.randint(10, 20) if which_small else np.random.randint(5)
+    width1 = np.random.randint(10, 25) if which_small else np.random.randint(5, 10)
     up_y1 = y1 + width1 + np.random.randint(max([-3, -width1 + 2]), 3)
     down_y1 = y1 - width1 - np.random.randint(max([-3, -width1 + 2]), 3)
-    width2 = np.random.randint(5) if which_small else np.random.randint(10, 20)
+    width2 = np.random.randint(5, 10) if which_small else np.random.randint(10, 25)
     up_y2 = y2 + width2 + np.random.randint(max([-3, 2 - width2]), 3)
     down_y2 = y2 - width2 - np.random.randint(max([-3, 2 - width2]), 3)
     a_up, b_up = gen_line_2_points(x1, up_y1, x2=x2, y2=up_y2)
@@ -46,33 +47,40 @@ def gen_triangle_2_points(x1, y1, x2, y2, x_bounds, y_bounds):
     new_y = (const - a_mid[0] * new_x) / a_mid[1]
     a_3, b_3 = gen_line_2_points(new_x, new_y, rise=a_mid[1], run=a_mid[0], rev=rev)
     constraints.append((a_3, b_3))
-    constraints = check_lines_against_bounds(a_up, b_up, a_down, b_down, x_bounds, y_bounds, constraints)
-    constraints = check_lines_against_bounds(a_up, b_up, a_3, b_3, x_bounds, y_bounds, constraints)
-    constraints = check_lines_against_bounds(a_3, b_3, a_down, b_down, x_bounds, y_bounds, constraints)
-    return constraints
+    pts = []
+    constraints, pts = check_lines_against_bounds(a_up, b_up, a_down, b_down, x_bounds, y_bounds, constraints, pts)
+    constraints, pts = check_lines_against_bounds(a_up, b_up, a_3, b_3, x_bounds, y_bounds, constraints, pts)
+    constraints, pts = check_lines_against_bounds(a_3, b_3, a_down, b_down, x_bounds, y_bounds, constraints, pts)
+    return constraints, pts
 
 
-def determine_constraint(a1, b1, a2, b2, line_a, line_b, x_bounds, y_bounds, constraints):
-    int1 = np.linalg.solve(np.array([a1, line_a]), np.array([b1, line_b]))
-    int2 = np.linalg.solve(np.array([a2, line_a]), np.array([b2, line_b]))
+def determine_constraint(a1, b1, a2, b2, line_a, line_b, x_bounds, y_bounds, constraints,  points):
+    int1 = tuple(np.linalg.solve(np.array([a1, line_a]), np.array([b1, line_b])))
+    int2 = tuple(np.linalg.solve(np.array([a2, line_a]), np.array([b2, line_b])))
+    if point_within_bounds(int1, x_bounds, y_bounds) and int1 not in points:
+        points.append(int1)
+    if point_within_bounds(int2, x_bounds, y_bounds) and int2 not in points:
+        points.append(int2)
     if point_within_bounds(int1, x_bounds, y_bounds) or point_within_bounds(int2, x_bounds, y_bounds):
         eqn = (line_a, line_b)
         if eqn not in constraints:
             constraints.append(eqn)
-    return constraints
+    return constraints, points
 
 
-def check_lines_against_bounds(a1, b1, a2, b2, x_bounds, y_bounds, constraints):
-    sol = np.linalg.solve(np.array([a1, a2]), np.array([b1, b2]))
+def check_lines_against_bounds(a1, b1, a2, b2, x_bounds, y_bounds, constrs, pts):
+    sol = tuple(np.linalg.solve(np.array([a1, a2]), np.array([b1, b2])))
+    if point_within_bounds(sol, x_bounds, y_bounds):
+        pts.append(sol)
     if sol[0] < x_bounds[0]:
-        constraints = determine_constraint(a1, b1, a2, b2, [-1, 0], -x_bounds[0], x_bounds, y_bounds, constraints)
+        constrs, pts = determine_constraint(a1, b1, a2, b2, [-1, 0], -x_bounds[0], x_bounds, y_bounds, constrs, pts)
     elif sol[0] > x_bounds[1]:
-        constraints = determine_constraint(a1, b1, a2, b2, [1, 0], x_bounds[1], x_bounds, y_bounds, constraints)
+        constrs, pts = determine_constraint(a1, b1, a2, b2, [1, 0], x_bounds[1], x_bounds, y_bounds, constrs, pts)
     if sol[1] < y_bounds[0]:
-        constraints = determine_constraint(a1, b1, a2, b2, [0, -1], -y_bounds[0], x_bounds, y_bounds, constraints)
+        constrs, pts = determine_constraint(a1, b1, a2, b2, [0, -1], -y_bounds[0], x_bounds, y_bounds, constrs, pts)
     elif sol[1] > y_bounds[1]:
-        constraints = determine_constraint(a1, b1, a2, b2, [0, 1], y_bounds[1], x_bounds, y_bounds, constraints)
-    return constraints
+        constrs, pts = determine_constraint(a1, b1, a2, b2, [0, 1], y_bounds[1], x_bounds, y_bounds, constrs, pts)
+    return constrs, pts
 
 
 def point_within_bounds(sol, x_bounds, y_bounds):
@@ -80,9 +88,15 @@ def point_within_bounds(sol, x_bounds, y_bounds):
 
 
 def plot_region(ax, pt1, pt2, x_bounds, y_bounds, line_color):
-    const = gen_triangle_2_points(pt1[0], pt1[1], pt2[0], pt2[1], x_bounds, y_bounds)
-    ax.plot(pt1[0], pt1[1], 'bo')
-    ax.plot(pt2[0], pt2[1], 'bo')
+    const, pts = gen_triangle_2_points(pt1[0], pt1[1], pt2[0], pt2[1], x_bounds, y_bounds)
+    points = np.random.rand(len(pts), 2)
+    for pos1 in range(0, len(points)):
+        for pos2 in range(0, len(points[0])):
+            points[pos1][pos2] = pts[pos1][pos2]
+    hull = ConvexHull(points)
+    plt.fill(points[hull.vertices, 0], points[hull.vertices, 1], 'lime', alpha=1)
+    #ax.plot(pt1[0], pt1[1], 'bo')
+    #ax.plot(pt2[0], pt2[1], 'bo')
     for pos in range(0, 3):
         c = const[pos]
         a = c[0]
@@ -102,10 +116,10 @@ def gen_point_queue(num_int_points, start, end, x_bounds, y_bounds):
     queue = [start]
     for _ in range(0, num_int_points):
         if queue[len(queue) - 1][0] < (x_bounds[0] + x_bounds[1])/2:
-            queue.append((np.random.randint((x_bounds[0] + x_bounds[1]) / 2, x_bounds[1]),
+            queue.append((np.random.randint(3*(x_bounds[0] + x_bounds[1]) / 4, x_bounds[1]),
                           np.random.randint(y_bounds[0], y_bounds[1])))
         else:
-            queue.append((np.random.randint(x_bounds[0], (x_bounds[0] + x_bounds[1]) / 2),
+            queue.append((np.random.randint(x_bounds[0], (x_bounds[0] + x_bounds[1]) / 4),
                           np.random.randint(y_bounds[0], y_bounds[1])))
     queue.append(end)
     return queue
@@ -121,15 +135,19 @@ def convert_constraints(const):
 
 
 if __name__ == "__main__":
+    start = (10, 10)
+    end = (90, 90)
     a_mats = []
     b_mats = []
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    q = gen_point_queue(4, (10, 10), (90, 90), (0, 100), (0, 100))
+    q = gen_point_queue(4, start, end, (0, 100), (0, 100))
     for pos in range(0, len(q) - 1):
-        a, b, = plot_region(ax, q[pos], q[pos+1], (0, 100), (0, 100), line_color=np.random.rand(3,))
+        a, b, = plot_region(ax, q[pos], q[pos+1], (0, 100), (0, 100), line_color="black")
         a_mats.append(a)
         b_mats.append(b)
+    ax.plot(start[0], start[1], 'bo')
+    ax.plot(end[0], end[1], 'bo')
     fig.show()
     print(a_mats)
     print(b_mats)
